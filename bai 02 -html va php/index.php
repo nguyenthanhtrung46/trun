b0a40a5b22afd9201f361f8f6193b23b4c3a3b17
nguyenthanhtrung46 <97218257+nguyenthanhtrung46@users.noreply.github.com>
2022-05-18 16:54:35 +0700

    Add files via upload

---
+++ bai 02 -html va php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bai 2 html va php</title>
+</head>
+<body>
+    <!-- Header -->
+    <?php require_once './inc/header.php' ?>
+    <?php require_once './inc/header.php' ?>
+    <h1>html va php</h1>
+    <?php
+    $languge = 'ngon ngu lap trinh php';
+    var_dump($languge);
+    ?>
+    <?php include './inc/content.php'; ?>
+    <?php include './inc/content.php'; ?>
+    <!-- Footer -->
+    <?php require_once './inc/footer.php' ?>
+    <?php require_once './inc/footer.php' ?>
+</body>
+</html>
